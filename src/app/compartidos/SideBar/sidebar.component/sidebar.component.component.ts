6fcdf3ee0ef3a5314324a5532a140befcc4d9c0f
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-07-04 18:00:20 -0300

    Creación de Sidebar

---
+++ src/app/compartidos/SideBar/sidebar.component/sidebar.component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar.component',
+  imports: [],
+  templateUrl: './sidebar.component.component.html',
+  styleUrl: './sidebar.component.component.css'
+})
+export class SidebarComponentComponent {
+
+}
