4685854748f6d01c176e45e3c59e4e3fe33d70e1
luciaburky <luciaburky1@gmail.com>
2025-07-10 10:52:55 -0300

    cambios generales

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideAnimations(),
+    provideToastr(),
+  ]
+})
   .catch((err) => console.error(err));
