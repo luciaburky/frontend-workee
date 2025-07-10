fd65cbcb7cf8b0d7bb0a4369ccd23d745e8eeabc
luciaburky <luciaburky1@gmail.com>
2025-07-10 11:11:27 -0300

    modificar pais

---
+++ src/app/admin/ABMPais/modificar-pais/modificar-pais.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modificar-pais',
+  imports: [],
+  templateUrl: './modificar-pais.component.html',
+  styleUrl: './modificar-pais.component.css'
+})
+export class ModificarPaisComponent {
+
+}
