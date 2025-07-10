7c4e044c59b1d4ca5866874c07422bdb446b43d3
luciaburky <luciaburky1@gmail.com>
2025-07-10 11:10:56 -0300

    listado paises

---
+++ src/app/admin/ABMPais/listado-paises/listado-paises.component.ts
@@ -0,0 +1,106 @@
+import { Component, OnInit } from '@angular/core';
+import { Pais } from '../pais';
+import { PaisService } from '../pais.service';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { CrearPaisComponent } from '../crear-pais/crear-pais.component';
+import { ModalService } from '../../../compartidos/modal/modal.service';
+import Swal from 'sweetalert2'
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  selector: 'app-listado-paises',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './listado-paises.component.html',
+  styleUrls: ['./listado-paises.component.css']
+})
+export class ListadoPaisesComponent implements OnInit{
+  paisList: Pais[] = []; // lista de paises filtrada segun la busqueda
+  paisListOriginal: Pais[] = []; // lista de paises completa, sin filtrar
+  // nuevoPais: Pais;
+  modalRef?: NgbModalRef;
+  filtro: string = '';
+
+
+  constructor(
+    private paisService: PaisService,
+    // // private router: Router,
+    private modalService: ModalService,
+  ) {  }
+
+  ngOnInit(): void {
+    this.paisService.findAll().subscribe(paises => {
+      this.paisListOriginal = paises;
+      this.paisList = [...paises];
+    });
+  }
+
+  // Creacion de pais
+  crearPais() {
+    this.modalRef = this.modalService.open(CrearPaisComponent, {
+      centered: true,
+    });
+  }
+  
+  // Modificacion de pais
+  modificarPais() {}
+  
+  // Habilitacion de pais
+  habilitarPais() {
+    Swal.fire({
+      text: "¿Desea habilitar el parámetro?",
+      icon: "success",
+      iconColor: "#70DC73",
+      showCancelButton: true,
+      confirmButtonColor: "#70DC73",
+      cancelButtonColor: "#697077",
+      confirmButtonText: "Sí, habilitar",
+      cancelButtonText: "Volver",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+    } else {
+
+    }});
+  }
+  
+  // Deshabilitacion de pais
+  deshabilitarPais() {
+     Swal.fire({
+      text: "¿Desea habilitar el parámetro?",
+      icon: "error",
+      iconColor: "#FF5252",
+      showCancelButton: true,
+      confirmButtonColor: "#FF5252",
+      cancelButtonColor: "#697077",
+      confirmButtonText: "Sí, deshabilitar",
+      cancelButtonText: "Volver",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+    } else {
+
+    }});
+  }
+
+  // Ver provincias asociadas
+  verProvincias() {}
+
+  // Buscar paises dentro del listado
+  buscarPaises() {
+    const texto = this.filtro.trim().toLowerCase();
+
+    if (texto === '') {
+      this.paisList = [... this.paisListOriginal ];
+      return;
+    }
+
+    this.paisList = this.paisListOriginal.filter(pais =>
+      pais.nombrePais?.toLowerCase().includes(texto)
+    );
+  }
+
+}
