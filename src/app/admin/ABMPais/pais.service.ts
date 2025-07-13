c8c6709337eb801f56152faf0d6002edb0a150d5
luciaburky <luciaburky1@gmail.com>
2025-07-13 11:18:42 -0300

    cambios en service

--- src/app/admin/ABMPais/pais.service.ts
+++ src/app/admin/ABMPais/pais.service.ts
@@ -8,13 +8,17 @@ import { Pais } from './pais';
 })
 export class PaisService {
   private url: string = 'http://localhost:9090/paises';
-  id = new BehaviorSubject<number | null>(null);
-  id$ = this.id.asObservable();
+  
+  idSubject = new BehaviorSubject<number | null>(null);
 
   constructor(private http: HttpClient) { };
 
   findAll(): Observable<Pais[]> {
     return this.http.get<Pais[]>(this.url);
+  }
+  
+  findById(idPais:number): Observable<Pais> {
+    return this.http.get<Pais>(`${this.url}/${idPais}`);
   }
   
   deshabilitar(idPais:number) {
@@ -29,9 +33,13 @@ export class PaisService {
     }
     return this.http.put<Pais>(`${this.url}/habilitar/${idPais}`, body);
   }
-
+  
+  setId(id: number) {
+    this.idSubject.next(id);
+  }
+  
   getId(){
-    return this.id$;
+    return this.idSubject.asObservable();
   }
   
   crearPais(nombrePais: string){
@@ -40,5 +48,12 @@ export class PaisService {
     }
     return this.http.post(this.url,body);
   }
-
+  
+  modificarPais(idPais: number, nombrePais: string) {
+    console.log("llego al modificar del service")
+    const body = {
+      "nombrePais": nombrePais
+    }
+    return this.http.put(`${this.url}/${idPais}`,body);
+  }
 }
