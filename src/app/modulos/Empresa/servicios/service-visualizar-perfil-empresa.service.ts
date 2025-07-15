45a0f1f3fa5fe65e84bd4a9cb64d2b6f890e9f6a
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-07-15 20:58:57 -0300

    Cambios empresa

--- src/app/modulos/Empresa/servicios/service-visualizar-perfil-empresa.service.ts
+++ src/app/modulos/Empresa/servicios/service-visualizar-perfil-empresa.service.ts
@@ -1,10 +1,17 @@
-import { BehaviorSubject } from 'rxjs';
+// service-visualizar-perfil-empresa.service.ts
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class ServiceVisualizarPerfilEmpresaService {
-  constructor() {}
+  private url = 'http://localhost:9090/empresas'; // Asegurate de que esta URL sea correcta
+
+  constructor(private http: HttpClient) {}
+
+  obtenerEmpresaPorId(empresaId: number): Observable<any> {
+    return this.http.get<any>(`${this.url}/${empresaId}`);
+  }
 }
