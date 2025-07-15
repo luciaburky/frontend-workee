e7f7a835156fc00ee2659335bb42e557fff097a8
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-07-15 18:20:31 -0300

    Creacion empresa

---
+++ src/app/modulos/Empresa/visualizar-perfil-empresa/visualizar-perfil-empresa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-perfil-empresa',
+  imports: [],
+  templateUrl: './visualizar-perfil-empresa.component.html',
+  styleUrl: './visualizar-perfil-empresa.component.css'
+})
+export class VisualizarPerfilEmpresaComponent {
+
+}
