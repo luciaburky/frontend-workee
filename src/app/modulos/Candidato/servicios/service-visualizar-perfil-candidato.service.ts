eae9b3345ff765b9c4268bded3508b3d4bf75038
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-07-15 21:00:32 -0300

    Creacion de perfil y carpetas candidato

---
+++ src/app/modulos/Candidato/servicios/service-visualizar-perfil-candidato.service.ts
@@ -0,0 +1,13 @@
+import { BehaviorSubject } from 'rxjs';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ServiceVisualizarPerfilCandidatoService {
+
+
+  constructor() {}
+
+}
---
+++ src/app/modulos/Candidato/visualizar-perfil-candidato/visualizar-perfil-candidato.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizar-perfil-candidato',
+  imports: [],
+  templateUrl: './visualizar-perfil-candidato.component.html',
+  styleUrl: './visualizar-perfil-candidato.component.css'
+})
+export class VisualizarPerfilCandidatoComponent {
+
+}
