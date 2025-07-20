8676ca72e5b3997caf12760794d37533344546fc
luciaburky <luciaburky1@gmail.com>
2025-07-20 11:44:59 -0300

    cambios en crear empleado

--- src/app/modulos/empresa/empleados/crear-empleado/crear-empleado.component.ts
+++ src/app/modulos/empresa/empleados/crear-empleado/crear-empleado.component.ts
@@ -15,7 +15,10 @@ export class CrearEmpleadoComponent implements OnInit {
   verContrasenia: boolean = false;
   empleadoForm: FormGroup;
   submitForm: boolean = false;
-  
+  backendEmailInvalido = false;
+  backendContraseniasNoCoinciden = false;
+  backendContraseniaCorta = false;
+
   constructor(
     private router: Router,
     private empleadoService: EmpleadoService,
@@ -24,27 +27,26 @@ export class CrearEmpleadoComponent implements OnInit {
       nombreEmpleado: new FormControl('', [Validators.required]),
       apellidoEmpleado: new FormControl('', [Validators.required]),
       puestoEmpleado: new FormControl('', [Validators.required]),
-      correoEmpleado: new FormControl('', [Validators.required]),
-      contraseniaEmpleado: new FormControl('', [Validators.required]),
-      repetirContraseniaEmpleado: new FormControl('', [Validators.required])
+      correoEmpleado: new FormControl('', [Validators.required, Validators.email]),
+      contraseniaEmpleado: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      repetirContraseniaEmpleado: new FormControl('', [Validators.required, Validators.minLength(8)])
     })
   }
   
   ngOnInit(): void {}
 
   isCampoInvalido(nombreCampo: string): boolean {
-    // console.log('entre a la funcion con ', nombreCampo)
     const control = this.empleadoForm.get(nombreCampo);
-    return !!(control?.hasError('required') && (control?.touched || this.submitForm));
+    return !!(control && control.invalid && (control.touched || this.submitForm));
   }
 
   enviarDatos() {
-    if (this.empleadoForm.invalid) {
-      this.empleadoForm.markAllAsTouched();
-      return;
-    }
-
+    this.backendEmailInvalido = false;
     
+    // if (this.empleadoForm.invalid) {
+    //   this.empleadoForm.markAllAsTouched();
+    //   return;
+    // }
     
     const nombreEmpleado = this.empleadoForm.get('nombreEmpleado')?.value;
     const apellidoEmpleado = this.empleadoForm.get('apellidoEmpleado')?.value;
@@ -63,22 +65,47 @@ export class CrearEmpleadoComponent implements OnInit {
         next: () => {
           this.submitForm = true;
           this.volverAListado();
-            Swal.fire({
-              toast: true,
-              position: "top-end",
-              icon: "success",
-              title: "El empleado se creó correctamente",
-              timer: 3000,
-              showConfirmButton: false,
-            })
+          Swal.fire({
+            toast: true,
+            position: "top-end",
+            icon: "success",
+            title: "El empleado se creó correctamente",
+            timer: 3000,
+            showConfirmButton: false,
+          })
         },
         error: (error) => {
-
+          console.log(error.error.message);
+          if (error.status === 400 && error.error.message === "Debe ser un correo válido") {
+            this.backendEmailInvalido = true;
+            this.empleadoForm.get('correoEmpleado')?.setErrors({ backend: true });
+          } else if (error.status === 400 && error.error.message === "Las contraseñas deben coincidir") {
+            this.backendContraseniasNoCoinciden = true;
+            this.empleadoForm.get('contraseniaEmpleado')?.setErrors({ backend: true });
+            this.empleadoForm.get('repetirContraseniaEmpleado')?.setErrors({ backend: true });
+          } else if (error.status === 400 && error.error.message === "La contraseña debe tener al menos 8 caracteres") {
+            this.backendContraseniaCorta = true;
+            this.empleadoForm.get('contraseniaEmpleado')?.setErrors({ backend: true });
+          }
         }
       })
-
-
   }
+
+  // erroresDeBackend(mensaje: string) {
+  //   const errores = mensaje.split(', ');
+  //   console.log(errores)
+  //   errores.forEach(error => {
+  //     if (error.includes('correo')) {
+  //       this.empleadoForm.get('correoEmpleado')?.setErrors({ backend: error });
+  //       console.log("setee el error bien")
+  //     } else if (error.includes('repetir')) {
+  //       this.empleadoForm.get('repetirContraseniaEmpleado')?.setErrors({ backend: error });
+  //     } else if (error.includes('contraseña')) {
+  //       this.empleadoForm.get('contraseniaEmpleado')?.setErrors({ backend: error });
+  //     }
+  //     console.log(this.empleadoForm.get('correoEmpleado')?.getError('backend'));
+  //   });
+  // }
 
   volverAListado(): void {
     this.router.navigate([`empleados`])
