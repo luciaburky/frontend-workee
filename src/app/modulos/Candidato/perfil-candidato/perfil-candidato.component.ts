63c2dcccc6a93bab80fe9fec6706432df281b537
luciaburky <luciaburky1@gmail.com>
2025-07-29 09:34:23 -0300

    cambios perfil candidato

--- src/app/modulos/Candidato/perfil-candidato/perfil-candidato.component.ts
+++ src/app/modulos/Candidato/perfil-candidato/perfil-candidato.component.ts
@@ -9,6 +9,13 @@ import { Pais } from '../../../admin/ABMPais/pais';
 import { Provincia } from '../../../admin/ABMProvincia/provincia';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Candidato } from '../candidato';
+import { ModalService } from '../../../compartidos/modal/modal.service';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { SeleccionHabilidadesComponent } from './seleccion-habilidades/seleccion-habilidades.component';
+import { ActivatedRoute } from '@angular/router';
+import { CandidatoService } from '../candidato.service';
+import { Habilidad } from '../../../admin/ABMHabilidad/habilidad';
+import { CandidatoHabilidad } from '../candidato-habilidad';
 
 @Component({
   selector: 'app-perfil-candidato',
@@ -22,37 +29,52 @@ export class PerfilCandidatoComponent implements OnInit {
   verContrasenia: boolean = false;
   mostrarCampoRepetir: boolean = true;
   repetirContrasenia = '';
-  candidato: Candidato = {
-    estadoBusqueda: {
-      nombreEstadoBusqueda: 'Buscando activamente'
-    } 
-  };
+  candidato: Candidato = {};
+  modalRef?: NgbModalRef;
 
   paises: Pais[] =[];
   provincias: Provincia[] = [];
+  provinciasDePais: Provincia[] = []; // se usa para mostrar las provincias segun el pais que se selecciono
   generos: Genero[] = [];
+  habilidades: CandidatoHabilidad[] = [];
+  paisSeleccionado?: Pais;
 
   constructor(
+    private candidatoService: CandidatoService,
     private paisService: PaisService,
     private provinciaService: ProvinciaService,
     private generoService: GeneroService,
+    private modalService: ModalService,
+    private route: ActivatedRoute,
   ) {}
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('idCandidato'));
+    this.candidatoService.findById(id).subscribe({
+      next: (data) => {
+        this.candidato = data;
+        this.paisSeleccionado = this.candidato.provincia?.pais;
+        this.filtrarProvinciasPorPais(this.candidato.provincia?.pais || null);
+      },
+      error: (error) => {
+        console.error('Error al obtener el candidato', error);
+      }
+    });
+    
     this.paisService.findAllActivos().subscribe({
       next: (data) => {
         this.paises = data;
-        console.log(this.paises)
+        // console.log(this.paises)
       },
       error: (error) => {
         console.error('Error al obtener paises', error);
       }
     })
-
+    
     this.provinciaService.findAllActivas().subscribe({
       next: (data) => {
         this.provincias = data;
-        console.log(this.provincias)
+        // console.log(this.provincias)
       },
       error: (error) => {
         console.error('Error al obtener provincias', error);
@@ -62,16 +84,36 @@ export class PerfilCandidatoComponent implements OnInit {
     this.generoService.findAllActivos().subscribe({
       next: (data) => {
         this.generos = data;
-        console.log(this.generos)
+        //console.log(this.generos)
       },
       error: (error) => {
         console.error('Error al obtener generos', error);
       }
     })
+    
+    this.habilidades = this.candidato.habilidades ?? [];
+    // console.log("habilidades: ", this.habilidades)
   }
 
   modificarCandidato() {
     this.modoEdicion = true;
+  }
+
+  seleccionarHabilidades() {
+    this.modalRef = this.modalService.open(SeleccionHabilidadesComponent, {
+      centered: true,
+      scrollable: true,
+      size: 'lg'
+    });
+
+    this.modalRef.componentInstance.habilidadesSeleccionadas = [...this.habilidades];
+
+    // PARA RECIBIR LAS HABILIDADES ACA Y ENVIARLAS EN LA REQUEST
+    // this.modalRef.result.then(
+    //   (habilidadesSeleccionadas) => {
+
+    //   }
+    // )
   }
 
   volver() {
@@ -97,5 +139,42 @@ export class PerfilCandidatoComponent implements OnInit {
     }
   }
 
+  enviarDatos() {
+    // segun el chat, tengo que setear el pais en la provincia, si fue cambiado
+    // if (this.candidato.provincia) {
+    //   this.candidato.provincia.pais = this.paisSeleccionado;
+    // }
+  }
+
+  compararGenero = (g1: Genero, g2: Genero) => g1 && g2 ? g1.id === g2.id : g1 === g2;
+  
+  compararPais = (p1: Pais, p2: Pais) => p1 && p2 ? p1.id === p2.id : p1 === p2;
+  
+  compararProvincia = (p1: Provincia, p2: Provincia) => p1 && p2 ? p1.id === p2.id : p1 === p2;
+
+  formatearFecha(fechaStr: string | undefined): string {
+    if (!fechaStr) return '';
+
+    const [anio, mes, dia] = fechaStr.split('-');
+    return `${dia}/${mes}/${anio}`;
+  }
+
+  filtrarProvinciasPorPais(paisSeleccionado: Pais | null) {
+    if (!paisSeleccionado) {
+      this.provinciasDePais = [];
+      this.candidato.provincia = undefined; // limpiar si no hay país seleccionado
+      return;
+    }
+
+    this.provinciasDePais = this.provincias.filter(
+      provincia => provincia.pais?.id === paisSeleccionado.id
+    );
+
+    this.paisSeleccionado = paisSeleccionado;
+
+    if (!this.provinciasDePais.some(p => p.id === this.candidato.provincia?.id)) {
+      this.candidato.provincia = undefined;
+    }
+  }
 
 }
