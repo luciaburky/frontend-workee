0937cef8f03aacb6c6d7942ad87de0e5a10cebe5
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-08-08 11:43:05 -0300

    FIREBASE Y LOGO NAVEGADOR

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { environment } from './Enviroments/enviroment';
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
@@ -10,6 +13,8 @@ bootstrapApplication(AppComponent, {
     ...(appConfig.providers || []),
     provideAnimations(),
     provideToastr(),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideStorage(() => getStorage()),
   ]
 })
   .catch((err) => console.error(err));
