72064e26f00528795e86598a4f72192ecf5fe347
Maximo Costa <maximo.costa@alumnos.frm.utn.edu.ar>
2025-08-31 21:46:53 -0300

    Sidebar app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponentComponent } from "./compartidos/SideBar/sidebar.component/sidebar.component.component";
 
 @Component({
   selector: 'app-root',
