3d3327bc4b38b33f33b165f7be63a9c264489914
luciaburky <luciaburky1@gmail.com>
2025-09-15 11:31:47 -0300

    spinner en listado de ofertas propias

--- src/app/modulos/gestion de ofertas/visualizar oferta/visualizar-ofertas-propias.component.ts
+++ src/app/modulos/gestion de ofertas/visualizar oferta/visualizar-ofertas-propias.component.ts
@@ -1,103 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { OfertaService } from '../../oferta/oferta.service';
-// import { Oferta } from '../../oferta/oferta';
-// import { EmpresaService } from '../../empresa/empresa/empresa.service';
-// import { EstadoOferta } from '../../../admin/ABMEstadoOferta/estado-oferta';
-// import { CommonModule } from '@angular/common';
-
-// @Component({
-//   selector: 'app-visualizar-ofertas-propias',
-//   standalone: true,                 // <- IMPORTANTE
-//   imports: [CommonModule],
-//   templateUrl: './visualizar-ofertas-propias.component.html',
-//   styleUrl: './visualizar-ofertas-propias.component.css'
-// })
-// export class VisualizarOfertasPropiasComponent implements OnInit {
-
-//   agregarEtapa() {
-//     throw new Error('Method not implemented.');
-//   }
-
-//   ofertas: Oferta[] = [];
-//   ofertasObtenidas: Oferta[] = [];
-//   idEmpresaObtenida!: number;
-
-//   // NO hace falta un array global de estados: cada oferta ya trae estadosOferta
-//   // estados: EstadoOferta[] = [];  // <- podés borrar si no lo usás
-
-//   constructor(
-//     private ofertaService: OfertaService,
-//     private empresaService: EmpresaService,
-//   ) {}
-
-//   ngOnInit(): void {
-//     if (this.empresaService) {
-//       console.log('entra a empresa service');
-//       this.empresaService.getidEmpresabyCorreo()?.subscribe({
-//         next: (idEmpresa) => {
-//           if (idEmpresa !== undefined && idEmpresa !== null) {
-//             this.idEmpresaObtenida = idEmpresa;
-//             console.log('id empresa obtenido: ', idEmpresa);
-//             this.ofertaService.getOfertasPorEmpresa(idEmpresa).subscribe({
-//               next: (ofertas) => {
-//                 this.ofertasObtenidas = ofertas;
-//                 console.log('Ofertas disponibles: ', this.ofertasObtenidas);
-//                 this.ofertasObtenidas = ofertas.map(i => ({
-//                   ...i,
-//                   // opcional: agregá un campo derivado para mostrar fácil
-//                   estadoNombre: this.getEstadoActual(i)?.nombreEstadoOferta ?? '—',
-//                 }) as any);
-//               },
-//               error: (err) => {
-//                 console.error('Error al obtener ofertas disponibles', err);
-//               }
-//             });
-//           } else {
-//             console.error('El id de empresa obtenido es undefined o null');
-//           }
-//         },
-//         error: (err) => {
-//           console.error('Error al obtener id de empresa por correo', err);
-//         }
-//       });
-//     }
-//   }
-
-
-//   getEstadoActual(oferta: Oferta): EstadoOferta | null {
-//   const lista = oferta.estadosOferta ?? []; //En realidad recorre OfertaEstadoOferta de la oferta que le pase
-//   if (!lista.length) return null;
-
-//   // 1) preferí el registro vigente (sin fecha de baja)
-//   const vigente = lista.find(e => !e.fechaHoraBaja);
-//   if (vigente?.estadoOferta) return vigente.estadoOferta; //Devuelve el OfertaEstadoOferta que no tenga la fechaHoraBaja
-
-//   // 2) si no hay vigente, tomá el último por fecha de alta
-//   const ordenados = [...lista].sort((a, b) =>
-//     new Date(a.fechaHoraAlta ?? 0).getTime() - new Date(b.fechaHoraAlta ?? 0).getTime() //Devuelve el OfertaEstadoOferta con le fecha fechaHoraAlta mas nueva
-//   );
-//   return ordenados.at(-1)?.estadoOferta ?? null;
-// }
-
-
-// estadoClase(oferta: Oferta): string {
-//   const cod = (this.getEstadoActual(oferta)?.codigo ?? '').toUpperCase();
-//   switch (cod) {
-//     case 'ABIERTA':     return 'is-abierta';
-//     case 'CERRADA':     return 'is-cerrada';
-//     case 'FINALIZADA':  return 'is-finalizada';
-//     default:            return 'is-neutro';
-//   }
-// }
-
-// estadoTexto(oferta: Oferta): string {
-//   return this.getEstadoActual(oferta)?.nombreEstadoOferta ?? 'Sin estado';
-// }
-
-
-
-// }
-
 import { Component, OnInit, Inject } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -113,13 +13,14 @@ import { Router, RouterModule } from '@angular/router';
 import { UsuarioService } from '../../seguridad/usuarios/usuario.service';
 import { SesionService } from '../../../interceptors/sesion.service';
 import { Empleado } from '../../empresa/empleados/empleado';
+import { SpinnerComponent } from '../../../compartidos/spinner/spinner/spinner.component';
 
 type OpcionEstado = { code: string; name: string };
 
 @Component({
   selector: 'app-visualizar-ofertas-propias',
   standalone: true,
-  imports: [CommonModule, FormsModule, MultiSelect, RouterModule], // <-- AGREGA MultiSelect
+  imports: [CommonModule, FormsModule, MultiSelect, RouterModule, SpinnerComponent], // <-- AGREGA MultiSelect
   templateUrl: './visualizar-ofertas-propias.component.html',
   styleUrl: './visualizar-ofertas-propias.component.css'
 })
@@ -145,6 +46,8 @@ export class VisualizarOfertasPropiasComponent implements OnInit {
 
   empleado?: Empleado;
 
+  isLoading: boolean = false;
+
   constructor(
     private ofertaService: OfertaService,
     private empresaService: EmpresaService,
@@ -157,6 +60,7 @@ export class VisualizarOfertasPropiasComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.isLoading = true;
     // restaurar texto/estados si hay (SSR-safe)
     if (this.isBrowser) {
       const txt = sessionStorage.getItem('textoOfertaPropias');
@@ -171,6 +75,7 @@ export class VisualizarOfertasPropiasComponent implements OnInit {
       // console.log("hola ", rol)
       if (!rol) {
         console.error("No se pudo obtener el rol del usuario");
+        this.isLoading = false;
         return;
       }
   
@@ -207,22 +112,27 @@ export class VisualizarOfertasPropiasComponent implements OnInit {
       console.error('El id de empresa es inválido');
       return;
     }
-
+    
     this.idEmpresaObtenida = idEmpresa;
-
+    
+    this.isLoading = true;
     this.ofertaService.getOfertasPorEmpresa(idEmpresa).subscribe({
       next: (ofertas) => {
         this.ofertasObtenidas = ofertas.map(i => ({
           ...i,
           estadoNombre: this.getEstadoActual(i)?.nombreEstadoOferta ?? '—',
         }) as any);
-
+        this.isLoading = false;
+        
         this.cargarOpcionesEstados();
         this.aplicarFiltro();
-
+        
         console.log('Ofertas disponibles: ', this.ofertasObtenidas);
       },
-      error: (err) => console.error('Error al obtener ofertas disponibles', err)
+      error: (err) => {
+        console.error('Error al obtener ofertas disponibles', err);
+        this.isLoading = false;
+      }
     });
   }
 
